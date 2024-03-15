060c1cbc82b2bea9f78453502b7ba83429ae1f12
jonathan <bcbg92@hotmail.com>
2024-03-15 19:32:59 +0100

    update error

--- src/Class/regexError.ts
+++ src/Class/regexError.ts
@@ -1,12 +1,12 @@
 export class RegexError extends Error {
 	regex: RegExp;
-	match: RegExpMatchArray | null;
+	input: string;
 
 	constructor(parameters: string, regex: RegExp, input: string) {
 		super(parameters);
 		this.regex = regex;
-		this.match = input.match(this.regex);
+		this.input = input
 		this.name = "RegexError";
-		this.stack=""
+		this.cause = "The regex is not satisfied";
 	}
 }

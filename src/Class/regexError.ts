b06c83169a81547aa070b188a5aeb829350d81ee
jonathan <bcbg92@hotmail.com>
2024-03-15 17:22:00 +0100

    remove this.name in error

--- src/Class/regexError.ts
+++ src/Class/regexError.ts
@@ -1,12 +1,11 @@
 export class RegexError extends Error {
-  regex: RegExp;
-  match: RegExpMatchArray | null;
-  default: string;
+	regex: RegExp;
+	match: RegExpMatchArray | null;
 
-  constructor(parameters: string, regex: RegExp, input: string) {
-    super(parameters);
-    this.regex = regex;
-    this.match = input.match(this.regex);
-    this.default = "RegexError";
-  }
+	constructor(parameters: string, regex: RegExp, input: string) {
+		super(parameters);
+		this.regex = regex;
+		this.match = input.match(this.regex);
+		this.name = "RegexError";
+	}
 }

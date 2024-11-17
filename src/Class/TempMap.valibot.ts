60fc43bab9f51d5408fac228ac4ed2f7ac7bd5fa
jonathan <bcbg92@hotmail.com>
2024-11-17 02:31:31 +0100

    bugs

--- src/Class/TempMap.valibot.ts
+++ src/Class/TempMap.valibot.ts
@@ -15,50 +15,62 @@ const setOfNORegex = new Set<string>();
 const NoRegex = {
   addToSet: v.pipe(
     v.string("must be a string inside the Set Please"),
-    v.regex(/^\[?[@\w:-]+\]*$/, "Must be a no regex like string:[string]"),
+    v.regex(
+      /^\[[@\w:-]+\]$|[@\w:-]+$/,
+      "Must be a no regex like [string]|string"
+    ),
     v.transform((noRegexStr) => setOfNORegex.add(noRegexStr))
   ),
   convertToArray: v.pipe(
     v.set(
       v.pipe(
         v.string("must be a string inside the Set Please"),
-        v.regex(/^\[?[@\w:-]+\]*$/, "Must be a no regex like string:[string]")
+        v.regex(
+          /^\[[@\w:-]+\]$|[@\w:-]+$/,
+          "Must be a no regex like string:[string]"
+        )
       )
     ),
-    v.transform((e) => Array.from(e)),
-    v.mapItems((e) => e.split(":")),
-    v.minLength(1)
+    v.transform((e) => Array.from(e, (e) => e.split(":")))
   ),
 };
 
-const isRegex = v.pipe(
-  v.string("must be a string inside the Set Please"),
-  v.custom<Regex>((input) =>
-    typeof input === "string"
-      ? /(?<before>^[@?\w:]+):\[(?<css>[%/@\-\w:,[\]]+)\]$/.test(input)
-      : false
+const isRegex = v.undefinedable(
+  v.pipe(
+    v.string("must be a string inside the Set Please"),
+    v.custom<Regex>((input) =>
+      typeof input === "string"
+        ? /(?<before>^[@?\w:]+):\[(?<css>[%/@\-\w:,[\]]+)\]$/.test(input)
+        : false
+    ),
+    v.transform((isRegexStr) => {
+      const match = isRegexStr.match(
+        /(?<before>^[@?\w:]+):\[(?<css>[%/@\-\w:,[\]]+)\]$/
+      );
+      const before = v.parse(
+        v.string("before is not string type"),
+        match?.groups?.before
+      );
+      const css = v.parse(
+        v.string("css is not a string type"),
+        match?.groups?.css
+      );
+      mapRegex.set(before, splitString(css));
+    }),
+    v.transform(processMapDoWhile)
   ),
-  v.transform((isRegexStr) => {
-    const match = isRegexStr.match(
-      /(?<before>^[@?\w:]+):\[(?<css>[%/@\-\w:,[\]]+)\]$/
-    );
-    const before = v.parse(
-      v.string("before is not string type"),
-      match?.groups?.before
-    );
-    const css = v.parse(
-      v.string("css is not a string type"),
-      match?.groups?.css
-    );
-    mapRegex.set(before, splitString(css));
-  }),
-  v.transform(processMapDoWhile)
+  "must be a string inside the Set Please"
 );
 
 export function init(Initial: Set<string>): Set<string> {
+  setOfNORegex.clear();
   for (const iterator of Initial) {
     v.parse(v.union([isRegex, NoRegex.addToSet]), iterator);
   }
+  console.log({
+    setOfNORegex,
+  });
+
   Initial.clear();
   processMapDoWhile();
   return setOfNORegex;
@@ -70,6 +82,8 @@ function processMapDoWhile(): void {
     iteration++;
     const TempSet = new Set<string>();
     mapRegex.forEach((value, key) => {
+      console.log({ value, key });
+
       for (const iterator of value) {
         TempSet.add(`${key}:${iterator}`);
       }
@@ -84,38 +98,64 @@ function processMapDoWhile(): void {
   }
 }
 
+const firstString = v.fallback(
+  v.pipe(
+    v.string("must be a string inside firstString"),
+    v.regex(/.*[,]+.+/g, "coma is missing in regex")
+  ),
+  "error firstString"
+);
 // /////////////////////////////////////////////
 /**
- * MARK: fonction builder
+ * MARK: function builder
  */
 // /////////////////////////////////////////////
-
 const processString = (x: string): Set<string> => {
-  const X = v.parse(v.string("must be string in processString"), x);
-  const Initial = splitString(X);
-  init(Initial);
-  return setOfNORegex;
+  const X = v.safeParse(firstString, x);
+  try {
+    const Initial = splitString(X.output as string);
+
+    init(Initial);
+    return setOfNORegex;
+  } catch (error) {
+    if (error instanceof v.ValiError) {
+      console.error(error.issues);
+      console.error(error.cause);
+    }
+    return new Set<string>(["error #1"]);
+  }
 };
 
-function p(category: string, value: string): string {
+function tailwindKiller2(category: Category, value: string): string {
   const setElement = processString(value);
   const arrayOf = v.parse(NoRegex.convertToArray, setElement);
-  arrayOf.map((e) => {
-    if (e.length > 1) {
+  try {
+    arrayOf.map((e) => {
       e.splice(e.length - 1, 0, category);
-    } else {
-      e.push(category);
-    }
-    return e;
-  });
-  const mergeBeforeCss = arrayOf
-    .flatMap((Element) => {
-      const Before = Element.slice(0, -1).join(":");
-      const CatCss = Element.pop();
-      return `${Before}-${CatCss}`;
-    })
-    .join(" ");
+      console.log(e);
+      return e;
+    });
+    console.log(arrayOf);
+    
+    const mergeBeforeCss = arrayOf
+      .flatMap((Element) => {
+        const Before = Element.slice(0, -1).join(":");
+        const CatCss = Element.pop();
+        return `${Before}-${CatCss}`;
+      })
+      .join(" ");
 
-  return mergeBeforeCss;
+    return mergeBeforeCss;
+  } catch (error) {
+    if (error instanceof v.ValiError) {
+      console.error(error.issues);
+      console.error(error.cause);
+    }
+    return "error tailwind killer2";
+  }
 }
-export default p;
+export default tailwindKiller2;
+
+//const f = tailwindKiller2("text", "1,,2,orange,3xl,md:hover:pink");
+//console.log(f);
+ 

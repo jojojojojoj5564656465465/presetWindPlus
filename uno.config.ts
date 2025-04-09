50d8c177d9ea0b69b1aba8fa8fbca06cf089dd2d
jonathan <bcbg92@hotmail.com>
2025-04-09 03:27:06 +0200

    format code

--- uno.config.ts
+++ uno.config.ts
@@ -3,8 +3,5 @@ import presetWind4 from "@unocss/preset-wind4";
 import {presetWindPlus} from "./src";
 
 export default defineConfig({
-	presets: [
-		presetWind4(),
-		presetWindPlus({ maxScreenW: 1150, minScreenW: 320 })
-	],
+	presets: [presetWind4(), presetWindPlus({ maxScreenW: 1150, minScreenW: 320 })],
 });
